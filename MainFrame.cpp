a37b86cc253a3f47d8e5668ba920feafb12daa0c
Jonathan Cameron <jmcameron@gmail.com>
2017-08-18 19:55:49 -0700

    Cleanups

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 
 #include <wx/wx.h>
+#include <wx/font.h>
 #include <wx/sizer.h>
 #include <wx/aboutdlg.h>
 
@@ -30,10 +31,28 @@ MainFrame::MainFrame(wxWindow* parent)
     const unsigned int gap_width = 2;
     wxGridSizer *grid_sizer = new wxGridSizer(num_nodes, num_cols, gap_width, gap_width);
 
+    // Figure out the cell size
+    wxFont pane_font(wxFontInfo(12).Family(wxFONTFAMILY_SWISS));
+
     // ??? Eventually figure out the cell_size from the format
-    const wxSize font_size = GetFont().GetPixelSize();
-    const wxSize cell_size(config.pane_width_chars*font_size.x, 
-			   config.pane_height_lines*font_size.y);
+#ifdef __WINDOWS__
+    // GetPixelSize() does not seem to work correctly on Windows
+    const unsigned int char_width = 15;
+    const unsigned int line_height = 24;
+#else
+    const wxSize font_size = pane_font.GetPixelSize();
+    const unsigned int char_width = font_size.x;
+    const unsigned int line_height = font_size.y;
+#endif // __WINDOWS__
+    const wxSize cell_size(config.pane_width_chars*char_width, 
+			   config.pane_height_lines*line_height);
+    /* DEBUG
+    std::stringstream smsg;
+    smsg << "\ncell size w,h= " << cell_size.x << "," << cell_size.y 
+	 << std::endl;
+    wxMessageDialog sdialog(NULL, smsg.str(), _("sizes"), wxICON_INFORMATION);
+    sdialog.ShowModal();
+    */
 
     // Create all the panes for the nodes
     for (NodeNameList::const_iterator nd = config.nodes.begin(); 
@@ -49,6 +68,7 @@ MainFrame::MainFrame(wxWindow* parent)
     {
 	const std::string node_name = *nd;
 	NodeDisplayPane *pane = new NodeDisplayPane(this, cell_size);
+	pane->SetFont(pane_font);
 	grid_sizer->Add(pane, 1, wxALL|wxFIXED_MINSIZE, 3);
 	node_displays[node_name] = pane;
 	nodes[node_name]->setNodeDisplay(pane);
